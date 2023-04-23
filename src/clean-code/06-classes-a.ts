7672e30ded9b6275a89573a13805c0f5aa4c5281
Raul Romero <link_2642@hotmail.com>
2023-04-22 20:34:05 -0600

    update: ejemplo de no responsabilidad unica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (()=> {
 
+    //Sin aplicar responsabilidad unica
+
     type Gender = 'M'|'F';
 
     class Person {
@@ -14,7 +16,49 @@
         }
     }
 
+    class User extends Person{
+        public lastAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ){
+            super(name,gender,birthdate);
+            this.lastAccess = new Date();
+        }
 
-    const newPerson = new Person('Fernando','M',new Date('1985-10-21'))
-    console.log({newPerson});
+        checkCredentiasl(){
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ){
+            super(email,role,name,gender,birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        'usr/home',
+        '/home',
+        'raul@google.com',
+        'Admin',
+        'Raul',
+        'M',
+        new Date('1996-08-26')
+    );
+
+    console.log({userSettings});
+
+
 })();
