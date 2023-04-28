5d43aa96291aa1e56447c6a8b01283d1c3c838af
RaulRomero26 <link_2642@hotmail.com>
2023-04-28 09:40:01 -0600

    update: aplicado principio dry

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -12,20 +12,17 @@
         ) {}
     }
 
-    class InputAttributes extends HtmlElement {
+    class InputAttributes {
         constructor(
             public value: string,
             public placeholder: string,
-            id: string,
-        ) {
-            super(id, 'input');
-        }
+           
+        ) {}
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
-        }
+    class InputEvents {
+
+        constructor() {}
 
         setFocus() {};
         getValue() {};
@@ -36,7 +33,20 @@
 
     //? Idea para la nueva clase InputElement
 
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
+    class InputElement {
+        public html: HtmlElement;
+        public attributes: InputAttributes;
+        public events: InputEvents;
+        constructor(value: string, placeholder: string, id: string){
+
+            this.html = new HtmlElement(id,'input');
+            this.attributes = new InputAttributes(value,placeholder);
+            this.events = new InputEvents();
+
+        }
+    }
+
+    const nameField = new InputElement('Fernando', 'Enter first name', 'txtName');
 
     console.log({ nameField });
 
